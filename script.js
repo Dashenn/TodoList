325635b77f3c5739d9d30fb2d2c06d578c2d6a67
Darya <d.lyadskaya@tg.dunice.net>
2024-07-25 17:57:20 +0300

    add function edit task, add task counter

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+
+
 let todoList = []
 const input = document.querySelector('.input-new-task')
 const addButton = document.querySelector('.task-add')
 const checkAll = document.querySelector('.all-check')
 const deleteCompleted = document.querySelector('.delete-all-completed')
+const todoContainer = document.querySelector('.todo-container')
+const spanAll = document.querySelector('.all')
+const spanActive = document.querySelector('.active')
+const spanCompleted = document.querySelector('.completed')
 const addTask = () => { 
     if (input.value === '') {
         return 
@@ -15,7 +21,7 @@ const addTask = () => {
         input.value = ''
         todoList.push(newTask)
         createList(todoList)
-
+        updateCounters()
    
 }
 const createList = (list) => {
@@ -24,13 +30,17 @@ const createList = (list) => {
     list.forEach(item => {
         const task = `
             <li data-id="${item.id}">
-                <input type="checkbox" name="check" class="check" ${item.completed}>
+            <div class='task-item'> 
+                <input type="checkbox" name="check" class="check" ${item.completed ? 'checked' : ''}>
                 <p class="task-text">${item.text}</p>
+                <input type='text' class='input-edit no-visible' value='${item.text}'/>
                 <button class="task-delete">Удалить</button>
+                </div>
             </li>
         `
         taskList.innerHTML += task
     })
+    updateCounters()
 }
 
 const updateTaskStatus = (id, checked) => {
@@ -40,22 +50,47 @@ const updateTaskStatus = (id, checked) => {
         }
     });
     createList(todoList)
+
 }
 
 const removeTask = (id) => {
     todoList = todoList.filter(item => item.id !== id)
     createList(todoList)
+    updateCounters()
 }
 
 const checkAllTasks = (checked) => {
     todoList.forEach(item => item.completed = checked)
     createList(todoList)
+    updateCounters()
 }
 
 const delCompleted = () => {
     todoList = todoList.filter(item => !item.completed);
     createList(todoList);
+    updateCounters()
 };
+
+const editTask = (id, newText) => {
+    todoList.forEach(task => {
+        if (task.id === id) {
+            task.text = newText;
+        }
+    });
+    createList(todoList);
+}
+
+const updateCounters = () => {
+    const totalCount = todoList.length
+    const completedCount = todoList.filter(task => task.completed).length
+    const activeCount = totalCount - completedCount
+    spanAll.textContent = `All (${totalCount})`
+    spanCompleted.textContent = `Completed (${completedCount})`
+    spanActive.textContent = `Active (${activeCount})`
+    
+}
+
+
 
 const changeTask = (event) => {
     
@@ -64,21 +99,72 @@ const changeTask = (event) => {
         addTask();
     } 
     if (event.target.matches('.check')) {
-        let id = parseInt(event.target.parentNode.dataset.id);
-        updateTaskStatus(id, event.target.checked);
+        let id = parseInt(event.target.parentNode.parentNode.dataset.id)
+        updateTaskStatus(id, event.target.checked)
     } 
     if (event.target.matches('.task-delete')) {
-        let id = parseInt(event.target.parentNode.dataset.id);
-        removeTask(id);
+        let id = parseInt(event.target.parentNode.parentNode.dataset.id)
+        removeTask(id)
     } 
     if (event.target=== checkAll) {
-        checkAllTasks(event.target.checked);
+        checkAllTasks(event.target.checked)
     } 
     if (event.target === deleteCompleted) {
-        delCompleted();
+        delCompleted()
+    }
+    if (event.target.matches('.task-text') &&event.type === 'dblclick') {
+        const listItem = event.target.parentNode.parentNode;
+        const editInput = listItem.querySelector('.input-edit')
+        const taskText = listItem.querySelector('.task-text')
+        editInput.classList.remove('no-visible')
+        editInput.classList.add('visible')
+        taskText.classList.add('no-visible')
+        
+    }
+    if (event.type === 'blur' && event.target.matches('.input-edit')) {
+        const listItem = event.target.parentNode.parentNode
+        const newText = event.target.value
+        const id = parseInt(listItem.dataset.id)
+        if (newText) {
+            editTask(id, newText)
+        } else {
+       
+            event.target.value = todoList.find(task => task.id === id).text
+        }
+        createList(todoList)
+    }
+    if (event.type === 'keyup' && event.key === 'Enter' && event.target.matches('.input-edit')) {
+        event.target.blur()
+    }
+
+    if (event.type === 'keyup' && event.key === 'Escape' && event.target.matches('.input-edit')) {
+        const listItem = event.target.parentNode.parentNode
+        const editInput = listItem.querySelector('.input-edit')
+        const taskText = listItem.querySelector('.task-text')
+        editInput.classList.add('no-visible')
+        editInput.classList.remove('visible')
+        taskText.classList.remove('no-visible')
+        editInput.value = taskText.textContent
+    }
+    if (event.target === spanAll) {
+        createList(todoList)
+        updateCounters()
+    }
+    if (event.target === spanActive) {
+        todoListActive = todoList.filter(item => item.completed === false)
+        createList(todoListActive)
+        updateCounters()
+    }
+    if (event.target === spanCompleted) {
+        todoListB = todoList.filter(item => item.completed === true)
+        createList(todoListB)
+        updateCounters()
     }
 };
 
 
-document.addEventListener('click', changeTask);
-document.addEventListener('keyup', changeTask);
+todoContainer.addEventListener('click', changeTask)
+todoContainer.addEventListener('dblclick', changeTask)
+todoContainer.addEventListener('keyup', changeTask)
+todoContainer.addEventListener('blur', changeTask, true)
+input.addEventListener('keyup', changeTask)
